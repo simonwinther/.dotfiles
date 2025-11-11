92cd3c0ca7da0493fcf8802da4958eeb2c77e8cd
Simon Winther <simonwa01@gmail.com>
2025-11-11 12:29:03 +0100

    cpp template change

--- nvim/.config/nvim/snippets/cpp.lua
+++ nvim/.config/nvim/snippets/cpp.lua
@@ -10,10 +10,12 @@ return {
     fmt(
       [[
 #include <bits/stdc++.h>
-
 using namespace std;
 
 int main() {{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     {}
     return 0;
 }}
